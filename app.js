86b250cd768902680ec12e326dabefd3bb29402c
ravindra rawat <ravinrawat.wd@gmail.com>
2019-02-17 12:17:11 -0700

    tuples enum

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 "use strict";
 // String
-var name = "ravraw";
+var somename = "ravraw";
+somename = "anothername";
+// number
+var someAge = 12;
+someAge = 14;
+// Boolean
+var isValid = true;
+isValid = false;
+// Another way 
+var someString;
+someString = "string";
+someString = "anotherString"; // no error
+// Arrays
+var numbers = ["one", "two"];
+numbers = ["1", "2"];
+// tuples
+var address = ["street", "house", 99, 99];
+// enum
+var Colors;
+(function (Colors) {
+    Colors[Colors["Red"] = 100] = "Red";
+    Colors[Colors["Green"] = 101] = "Green";
+    Colors[Colors["Blue"] = 102] = "Blue";
+})(Colors || (Colors = {}));
+var myColor = Colors.Blue;
+console.log(myColor);
--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ let somename:string = "ravraw";
 somename = "anothername";
 // number
 let someAge:number = 12;
-age = 14;
+someAge = 14;
 // Boolean
 let isValid:boolean = true;
 isValid = false;
@@ -13,3 +13,22 @@ isValid = false;
 let someString: string;
 someString = "string";
 someString = "anotherString"; // no error
+
+// Arrays
+
+let numbers:string[] = ["one","two"];
+numbers = ["1","2"];
+
+// tuples
+let address: [string,string,number,number] = ["street","house",99,99];
+
+// enum
+
+enum Colors  {
+Red=100,
+Green,
+Blue
+}
+
+let myColor :Colors=Colors.Blue;
+console.log(myColor);
